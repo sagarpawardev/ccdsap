389e46f297731dd47bdb12001ce39f6809982d10
sagarpawardev <sagar.pawar.dev@gmail.com>
2020-03-26 23:12:59 +0530

    IPCTRAIN

---
+++ heaps/ANUMLA.cpp
@@ -0,0 +1,69 @@
+//
+// Created by Sagar Pawar on 25/03/20.
+//
+/**
+ * Basic Logic: sort the array. And generate all possible values in ascending order. Separate all unit set from all composite sets
+ *
+ * Sort the array
+ * Check if value is exist in composite set
+ * If not composite:
+ *  calculate all possible values using unit set and composite set
+ *  put value in unit set
+ *
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    /*char* testFile = "/Users/sagarpawar/CLionProjects/ccdsap/heaps/test/ANUMLA.txt";
+    freopen(testFile, "r+", stdin);*/
+
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(pow(2,n));
+        for(int i=0; i<pow(2, n); i++){
+            cin >> a[i];
+        }
+
+        //Sort array
+        sort(a.begin(), a.end());
+
+        vector<int> unitSet;
+        unordered_set<int> compositeSet;
+        //skip 0
+        for(int i=1; i<a.size(); i++){
+
+            //find if value exist in composite set
+            int elem = a[i];
+            if(compositeSet.find(elem) == compositeSet.end()){
+
+                //Add all possible combination in composite set
+                unordered_set<int> temp;
+                for(int singleElem: unitSet){
+                    temp.insert(singleElem + elem );
+                }
+                for(int compElem: compositeSet){
+                    temp.insert(compElem + elem);
+                }
+                compositeSet.insert(temp.begin(), temp.end());
+
+                //Add value in unit set
+                unitSet.push_back(elem);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            cout << unitSet[i] << " ";
+        }
+        cout << endl;
+    }
+
+}
--- heaps/IPCTRAIN.cpp
+++ heaps/IPCTRAIN.cpp
@@ -2,9 +2,65 @@
 // Created by Sagar Pawar on 22/03/20.
 //
 #include <bits/stdc++.h>
+#define ll long long
 
 using namespace std;
 
+
+bool compare (vector<int> a, vector<int> b){
+    return a[0] < b[0];
+}
+
+bool descSadness(vector<int> a, vector<int> b){
+    return a[2] < b[2];
+}
+
 int main(){
-    cout << "Hello";
+    /*char* testFile = "/Users/sagarpawar/CLionProjects/ccdsap/heaps/test/IPCTRAIN.txt";
+    freopen(testFile, "r+", stdin);*/
+
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, d;
+        cin >> n >> d;
+
+        vector< vector<int> > a(n);
+        for(int i=0; i<n; i++){
+            vector<int> arr(3);
+            cin >> arr[0] >> arr[1] >> arr[2];
+
+            a[i] = arr ;
+        }
+
+        sort(a.begin(), a.end(), compare );
+        vector< vector<int> > heap;
+        int j = 0;
+        for(int i=1; i<=d; i++) {
+            //cout << a[i][0] << " " << a[i][1] << " " << a[i][2] << endl;
+            while(j<n && a[j][0]<=i){
+                heap.push_back(a[j++]);
+                push_heap(heap.begin(), heap.end(), descSadness);
+            }
+
+            if(!heap.empty()) {
+                heap[0][1]--;
+
+                if (heap[0][1] <= 0) {
+                    pop_heap(heap.begin(), heap.end(), descSadness);
+                    heap.pop_back();
+                }
+            }
+
+        }
+
+        ll sadness = 0;
+        for(vector<int> e: heap){
+            sadness += ((ll)e[1] * (ll)e[2]);
+        }
+
+        cout << sadness << endl;
+    }
+
 }

49849813139880d04db88c928123f855ae084ef4
sagarpawardev <sagar.pawar.dev@gmail.com>
2020-04-03 00:09:34 +0530

    RRATING: Restaurant Rating

--- heaps/RRATING.cpp
+++ heaps/RRATING.cpp
@@ -1,7 +1,22 @@
 //
 // Created by Sagar Pawar on 02/04/20.
 //
-
+/**
+ * Problem : RRATING
+ *
+ * Algorithm :
+ * 1. If insert query then put in default array
+ * 2. If show query then push to result array until 1/3rd of total capacity reached
+ *      or least element from 1/3rd is less than greatest element of default array
+ * 3. Clear the over-sized top 1/3rd element
+ *
+ * Complexity: O( n(logn + nlogn) ) = O(n^2 logn)
+ *
+ * Learning :
+ * 1. Process query lazily
+ * 2. Time ( Push smaller heap -> bigger heap ) > Time (Push bigger heap -> smaller heap)
+ *
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -14,51 +29,56 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> heap;
-    vector<int> buffer;
+    vector<int> topOneThird;
+    vector<int> defaultHeap;
     int size = 0;
     while (n--) {
         int q;
         cin >> q;
+
+        //If query type insert
         if (q == 1) {
             int x;
             cin >> x;
 
             size++;
 
-            buffer.push_back(x);
-            push_heap(buffer.begin(), buffer.end());
+            //Push to default heap
+            defaultHeap.push_back(x);
+            push_heap(defaultHeap.begin(), defaultHeap.end());
         } else {
 
-            int maxSize = size / 3;
+            int maxCapacity = size / 3;
 
-            while ((!buffer.empty() && !heap.empty() && (heap[0] < buffer[0])) || (heap.size() < maxSize)) {
-                int x = buffer[0];
+            // Pop values from default heap to topOneThird if
+            // a) topOneThird's smallest value < default heap greatest value or
+            // b) topOneThird's size has not reached maxCapacity
+            while ((!defaultHeap.empty() && !topOneThird.empty() && (topOneThird[0] < defaultHeap[0])) || (topOneThird.size() < maxCapacity)) {
+                int x = defaultHeap[0];
 
-                pop_heap(buffer.begin(), buffer.end());
-                buffer.pop_back();
+                pop_heap(defaultHeap.begin(), defaultHeap.end());
+                defaultHeap.pop_back();
 
-                heap.push_back(x);
-                push_heap(heap.begin(), heap.end(), greater<>());
-                //cout << "Hello" << endl;
+                topOneThird.push_back(x);
+                push_heap(topOneThird.begin(), topOneThird.end(), greater<>());
             }
 
-            while (heap.size() > maxSize) {
-                int x = heap[0];
+            //Push values to default heap from topOneThird in case of over capacity
+            while (topOneThird.size() > maxCapacity) {
+                int x = topOneThird[0];
 
-                pop_heap(heap.begin(), heap.end(), greater<>());
-                heap.pop_back();
+                pop_heap(topOneThird.begin(), topOneThird.end(), greater<>());
+                topOneThird.pop_back();
 
-                buffer.push_back(x);
-                push_heap(buffer.begin(), buffer.end());
-                //cout << "Sagar" << endl;
+                defaultHeap.push_back(x);
+                push_heap(defaultHeap.begin(), defaultHeap.end());
             }
 
 
-            if (heap.empty()) {
+            if (topOneThird.empty()) {
                 cout << "No reviews yet";
             } else {
-                cout << heap[0];
+                cout << topOneThird[0];
             }
             cout << endl;
         }

4ae1b54a12e33a40aca6f94845c45c6bf8bf1455
sagarpawardev <sagar.pawar.dev@gmail.com>
2020-04-04 00:18:40 +0530

    TSECJ05: Chef and his software

---
+++ heaps/TSECJ05.cpp
@@ -0,0 +1,66 @@
+//
+// Created by Sagar Pawar on 04/04/20.
+//
+
+/**
+ * Problem : TSECJ05
+ *
+ * Algorithm :
+ * 1. For each operation
+ * 2. If heap is less than required size insert element
+ * 3. If incoming value is greater than smallest value of heap then pop from heap and insert incoming value
+ *
+ * Complexity: O( nlogk )
+ *
+ * Learning :
+ * 1. Basic heap usage
+ *
+ */
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    /*char *testFile = "/Users/sagarpawar/CLionProjects/ccdsap/heaps/test/TSECJ05.txt";
+    freopen(testFile, "r+", stdin);*/
+
+    ios::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int k, n;
+        cin >> k >> n;
+
+        vector<int> heap;
+        for(int i=0; i<n ;i++){
+            int ni;
+            cin >> ni;
+
+            if(heap.size()<k){
+                heap.push_back(ni);
+                push_heap(heap.begin(), heap.end(), greater<>());
+            }
+            else if(heap[0]<ni){
+                pop_heap(heap.begin(), heap.end(), greater<>());
+                heap.pop_back();
+
+                heap.push_back(ni);
+                push_heap(heap.begin(), heap.end(), greater<>());
+            }
+
+            if(heap.size()<k){
+                cout << "-1" << " ";
+            }
+            else{
+                cout << heap[0] << " ";
+            }
+        }
+
+        cout << endl;
+
+    }
+}

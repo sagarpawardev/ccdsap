bad799b8fc8da8ce52b27eb1094dfe3a5cf47ae3
sagarpawardev <sagar.pawar.dev@gmail.com>
2020-04-11 02:50:51 +0530

    APRIL20B: CARSELL and COVIDLQ

---
+++ APRIL20B/CARSELL.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Sagar Pawar on 11/04/20.
+//
+#define ll long long
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+    char *testFile = "/Users/sagarpawar/CLionProjects/ccdsap/APRIL20B/test/CARSELL.txt";
+    freopen(testFile, "r", stdin);
+#endif
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> price(n);
+        for (int i = 0; i < n; i++) {
+            cin >> price[i];
+        }
+
+        sort(price.begin(), price.begin() + n, greater<>());
+
+        ll sum = 0;
+        for (int day = 0; day < n; day++) {
+            sum = (sum + max( price[day] - day, 0)) % 1000000007;
+        }
+
+        cout << sum << "\n";
+    }
+
+}
---
+++ APRIL20B/COVIDLQ.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Sagar Pawar on 11/04/20.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    char *testFile = "/Users/sagarpawar/CLionProjects/ccdsap/APRIL20B/test/COVIDLQ.txt";
+    freopen(testFile, "r", stdin);
+#endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        bool following = true;
+        int prev = -100;
+        for(int i=0; i<n; i++){
+            int a;
+            cin >> a;
+
+            if(following && a == 1){
+                if(i-prev < 6){
+                    following = false;
+                }
+                prev = i;
+            }
+        }
+
+        cout << (following ? "YES" : "NO") << "\n";
+    }
+
+}
